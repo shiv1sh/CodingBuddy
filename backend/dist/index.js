ec04ae9327ce95c9817b4ca172d62c4cd6eaaf00
shiv1sh <shivkasht2001@gmail.com>
2025-02-08 18:58:05 +0530

    Implemented the Template endpoint

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,72 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
-console.log(process.env.CLAUDE_API_KEY);
+const sdk_1 = __importDefault(require("@anthropic-ai/sdk"));
+const prompts_1 = require("./prompts");
+const express_1 = __importDefault(require("express"));
+const node_1 = require("./default_pompts/node");
+const react_1 = require("./default_pompts/react");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const anthropic = new sdk_1.default({
+// defaults to process.env["ANTHROPIC_API_KEY"]
+});
+app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("Control came here");
+    const prompt = req.body.prompt;
+    const response = yield anthropic.messages.create({
+        messages: [{
+                role: 'user', content: prompt
+            },],
+        model: 'claude-3-5-sonnet-20241022',
+        max_tokens: 200,
+        system: "Return either react or node based on what do you think this project should be. Only return a single word either 'node' or 'react. Do not return any thing extra'"
+    });
+    const answer = response.content[0].text; // react or node
+    if (answer == "react") {
+        res.json({
+            prompts: [prompts_1.BASE_PROMPT, react_1.basePrompt]
+        });
+        return;
+    }
+    else if (answer === "node") {
+        res.json({
+            prompt: [node_1.basePrompt]
+        });
+        return;
+    }
+    res.json({
+        message: "Please give a detailed Prompt"
+    });
+}));
+app.listen(3000, () => {
+    console.log("Server started at port 3000");
+});
+// async function makeAnthropicCall() {
+//     await anthropic.messages.stream({
+//         messages: [{
+//             role: 'user', content: BASE_PROMPT
+//         },{
+//             role: 'user', content: "Here is an artifact that contains all files of the project visible to you. \nConsider the contents of ALL files in the project.\n\n{{BASE_PROMPT}} \n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n .gitignore\n- package-lock.json\n .bolt/prompt"
+//         },{
+//             role:'user', content:"Create a todo app"
+//         }],
+//         model: 'claude-3-5-sonnet-20241022',
+//         max_tokens: 1024,
+//         system: getSystemPrompt()
+//     }).on('text', (text) => {
+//         console.log(text);
+//     });
+// }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,62 @@
 require("dotenv").config()
 import Anthropic from "@anthropic-ai/sdk";
-import { getSystemPrompt } from "./prompts";
+import { BASE_PROMPT, getSystemPrompt } from "./prompts";
+import express from "express";
+import { TextBlock } from "@anthropic-ai/sdk/resources";
+import { basePrompt as node_basePrompt} from "./default_pompts/node";
+import { basePrompt as react_basePrompt} from "./default_pompts/react";
 
-
+const app = express();
+app.use(express.json());
 const anthropic = new Anthropic({
     // defaults to process.env["ANTHROPIC_API_KEY"]
 });
-
-async function makeAnthropicCall() {
-    await anthropic.messages.stream({
+app.post("/template",async (req,res)=>{
+    const prompt = req.body.prompt;
+    const response = await anthropic.messages.create({
         messages: [{
-            role: 'user', content: "For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos. \n\nUse stock photos from unsplash where appropriate, only valid URLs you know exist. Do not download the images, only link to them in image tags.\n\n"
-        },{
-            role: 'user', content: "Here is an artifact that contains all files of the project visible to you. \nConsider the contents of ALL files in the project.\n\n{{BASE_PROMPT}} \n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n .gitignore\n- package-lock.json\n .bolt/prompt"
-        },{
-            role:'user', content:"Create a todo app"
-        }],
+            role: 'user', content: prompt
+        },],
         model: 'claude-3-5-sonnet-20241022',
-        max_tokens: 1024,
-        system: getSystemPrompt()
-    }).on('text', (text) => {
-        console.log(text);
-    });
+        max_tokens: 200,
+        system: "Return either react or node based on what do you think this project should be. Only return a single word either 'node' or 'react. Do not return any thing extra'"
+    })
+    
+    const answer = (response.content[0] as TextBlock).text; // react or node
+    if(answer=="react"){
+        res.json({
+            prompts: [BASE_PROMPT,`Here is an artifact that contains all files of the project visible to you. \nConsider the contents of ALL files in the project.\n\n${react_basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n .gitignore\n- package-lock.json\n `],
+            uiPrompt:react_basePrompt
+        })
+        return;
+    }else if(answer==="node"){
+       res.json({
+        prompt:[`Here is an artifact that contains all files of the project visible to you. \nConsider the contents of ALL files in the project.\n\n${node_basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n .gitignore\n- package-lock.json\n`],
+        uiPrompt:node_basePrompt
+       })
+       return;
+    }
+    res.json({
+        message:"Please give a detailed Prompt"
+    })
+})
+app.listen(3000,()=>{
+    console.log("Server started at port 3000");
+})
+// async function makeAnthropicCall() {
+//     await anthropic.messages.stream({
+//         messages: [{
+//             role: 'user', content: BASE_PROMPT
+//         },{
+//             role: 'user', content: "Here is an artifact that contains all files of the project visible to you. \nConsider the contents of ALL files in the project.\n\n{{BASE_PROMPT}} \n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n .gitignore\n- package-lock.json\n .bolt/prompt"
+//         },{
+//             role:'user', content:"Create a todo app"
+//         }],
+//         model: 'claude-3-5-sonnet-20241022',
+//         max_tokens: 1024,
+//         system: getSystemPrompt()
+//     }).on('text', (text) => {
+//         console.log(text);
+//     });
 
-}
+// }
