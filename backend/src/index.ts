6dee219a84bb9c3d311747f13dac73568a4fc933
shiv1sh <shivkasht2001@gmail.com>
2025-02-08 13:40:29 +0530

    Engineered User and System Prompt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,26 @@
 require("dotenv").config()
 import Anthropic from "@anthropic-ai/sdk";
+import { getSystemPrompt } from "./prompts";
 
 
 const anthropic = new Anthropic({
-  // defaults to process.env["ANTHROPIC_API_KEY"]
+    // defaults to process.env["ANTHROPIC_API_KEY"]
 });
 
 async function makeAnthropicCall() {
-    const msg = await anthropic.messages.create({
-        model: "claude-3-Opus",
-        max_tokens: 8192,
-        temperature: 1, // the amount of randomeness in response (1 will give different form of response each time asked)
+    await anthropic.messages.stream({
         messages: [{
-            "role" : "user",
-            "content" : "what is 2+2 ?"
-        }]
+            role: 'user', content: "For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos. \n\nUse stock photos from unsplash where appropriate, only valid URLs you know exist. Do not download the images, only link to them in image tags.\n\n"
+        },{
+            role: 'user', content: "Here is an artifact that contains all files of the project visible to you. \nConsider the contents of ALL files in the project.\n\n{{BASE_PROMPT}} \n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n .gitignore\n- package-lock.json\n .bolt/prompt"
+        },{
+            role:'user', content:"Create a todo app"
+        }],
+        model: 'claude-3-5-sonnet-20241022',
+        max_tokens: 1024,
+        system: getSystemPrompt()
+    }).on('text', (text) => {
+        console.log(text);
     });
-    console.log(msg);
+
 }
